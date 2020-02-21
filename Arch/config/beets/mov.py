73cf679418b1d4e22420f44a51fbd435d33171eb
toad <47069617+toadsucks@users.noreply.github.com>
2020-02-20 19:02:59 -0600

    Update mov.py

--- Arch/config/beets/mov.py
+++ Arch/config/beets/mov.py
@@ -1,37 +1,41 @@
+# This program will only work if your naming scheme has quality in []
+# The program checks for all text up until the [] to determine if the
+# folders are a match. For example, if the file name is:
+#       B4E (2017) [24-Bit FLAC]
+# The program will determine matches with: B4E (2017)
+# Any text after the [] will not be used to determine matches.
+
+import collections
 import os
 import re
 import shutil
 import sys
-from pathlib import Path
-from soundfile import SoundFile
 
 raw_path = eval(sys.argv[1]).decode('utf-8')
-if re.search("\[\d\d-Bit\sFLAC\]", raw_path) is not None:
-    path_16 = re.search(r"(.+)\[\d\d-Bit\sFLAC\]", raw_path).group(1) + "[16-Bit FLAC]"
-    path_24 = re.search(r"(.+)\[\d\d-Bit\sFLAC\]", raw_path).group(1) + "[24-Bit FLAC]"
-    bit_16 = []
-    bit_24 = []
 
-    for filename in Path(path_16).rglob('*.[fF][lL][aA][cC]'):
-        with SoundFile(filename) as f:
-            if f.subtype == "PCM_16":
-                bit_16.append(filename)
-            if f.subtype == "PCM_24":
-                bit_24.append(filename)
+path_no_type = re.search(r".*[\\\/](.*)\[", raw_path).group(1)
+count = dict()
 
-    for filename in Path(path_24).rglob('*.[fF][lL][aA][cC]'):
-        with SoundFile(filename) as f:
-            if f.subtype == "PCM_16":
-                bit_16.append(filename)
-            if f.subtype == "PCM_24":
-                bit_24.append(filename)
+for multi_folder in next(os.walk(os.path.dirname(raw_path)))[1]:
+    if re.search(r'{}\[.*\]'.format(re.escape(path_no_type)), multi_folder):
+        for single_folder, _, file_names in os.walk(os.path.join(os.path.dirname(raw_path), multi_folder)):
+            for f in file_names:
+                if multi_folder in count:
+                    count[os.path.join(os.path.dirname(raw_path), multi_folder)] += 1
+                else:
+                    count[os.path.join(os.path.dirname(raw_path), multi_folder)] = 1
 
-    if len(bit_16) > 0 and len(bit_24) > 0:
-        if len(bit_16) > len(bit_24):
-            for f in os.listdir(path_24):
-                shutil.move(os.path.join(path_24, f), path_16)
-            os.rmdir(path_24)
-        else:
-            for f in os.listdir(path_16):
-                shutil.move(os.path.join(path_16, f), path_24)
-            os.rmdir(path_16)
+sorted_count = collections.OrderedDict(sorted(count.items()))
+
+for multi_folder in next(os.walk(os.path.dirname(raw_path)))[1]:
+    if re.search(r'{}\[.*\]'.format(re.escape(path_no_type)), multi_folder):
+        for single_folder, _, file_names in os.walk(os.path.join(os.path.dirname(raw_path), multi_folder)):
+            for f in file_names:
+                try:
+                    shutil.move(os.path.abspath(os.path.join(single_folder, f)), os.path.abspath(list(sorted_count.keys())[-1]))
+                except shutil.Error:
+                    pass
+
+for path in list(sorted_count.keys())[:-1]:
+    for _, folders, _ in os.walk(os.path.join(os.path.dirname(raw_path), path), topdown=False):
+        os.rmdir(os.path.join(os.path.dirname(raw_path), path, *folders))
