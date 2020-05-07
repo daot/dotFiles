c9ad0e5005a0ae08a21403e2a6dfa0a8ea1aa838
toad <47069617+toadsucks@users.noreply.github.com>
2020-05-07 12:07:58 -0500

    Update mov.py

--- Arch/config/beets/mov.py
+++ Arch/config/beets/mov.py
@@ -1,9 +1,8 @@
-# This program will only work if your naming scheme has quality in []
+# This program will only work if your naming scheme has info in []
 # The program checks for all text up until the [] to determine if the
 # folders are a match. For example, if the file name is:
 #       B4E (2017) [24-Bit FLAC]
 # The program will determine matches with: B4E (2017)
-# Any text after the [] will not be used to determine matches.
 
 import collections
 import os
@@ -11,31 +10,86 @@ import re
 import shutil
 import sys
 
-raw_path = eval(sys.argv[1]).decode('utf-8')
+# https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            if fullPath.lower().endswith(
+                (
+                    "mp3",
+                    "aac",
+                    "m4a",
+                    "mp4",
+                    "flac",
+                    "ogg",
+                    "opus",
+                    "ape",
+                    "wv",
+                    "mpc",
+                    "wma",
+                    "aiff",
+                )
+            ):
+                allFiles.append(fullPath)
+    return allFiles
 
-path_no_type = re.search(r".*[\\\/](.*)\[", raw_path).group(1)
+
+# raw_path = eval(sys.argv[1]).decode("utf-8")
+raw_path = sys.argv[1]
+
+path_no_type = re.search(r".*[\\\/](.*\(\d*\))", raw_path).group(1)
 count = dict()
 
 for multi_folder in next(os.walk(os.path.dirname(raw_path)))[1]:
-    if re.search(r'{}\[.*\]'.format(re.escape(path_no_type)), multi_folder):
-        for single_folder, _, file_names in os.walk(os.path.join(os.path.dirname(raw_path), multi_folder)):
-            for f in file_names:
-                if multi_folder in count:
-                    count[os.path.join(os.path.dirname(raw_path), multi_folder)] += 1
-                else:
-                    count[os.path.join(os.path.dirname(raw_path), multi_folder)] = 1
+    if re.search(r"{}.*".format(re.escape(path_no_type)), multi_folder):
+        folder_path = os.path.join(os.path.dirname(raw_path), multi_folder)
+        count[folder_path] = len(getListOfFiles(folder_path))
 
-sorted_count = collections.OrderedDict(sorted(count.items()))
+sorted_count = {k: v for k, v in sorted(count.items(), key=lambda item: item[1])}
 
-for multi_folder in next(os.walk(os.path.dirname(raw_path)))[1]:
-    if re.search(r'{}\[.*\]'.format(re.escape(path_no_type)), multi_folder):
-        for single_folder, _, file_names in os.walk(os.path.join(os.path.dirname(raw_path), multi_folder)):
-            for f in file_names:
-                try:
-                    shutil.move(os.path.abspath(os.path.join(single_folder, f)), os.path.abspath(list(sorted_count.keys())[-1]))
-                except shutil.Error:
-                    pass
+for release_folder in list(sorted_count)[:-1]:
+    for folder_file in getListOfFiles(release_folder):
+        try:
+            os.makedirs(
+                os.path.join(
+                    list(sorted_count)[-1],
+                    re.search(
+                        r"{}[\\\/](.*)[\\\/]".format(re.escape(release_folder)),
+                        folder_file,
+                    ).group(1)
+                    if re.search(
+                        r"{}[\\\/](.*)[\\\/]".format(re.escape(release_folder)),
+                        folder_file,
+                    )
+                    else "",
+                ),
+                exist_ok=True,
+            )
+            shutil.move(
+                folder_file,
+                os.path.join(
+                    list(sorted_count)[-1],
+                    re.search(
+                        r"{}[\\\/](.*)".format(re.escape(release_folder)), folder_file
+                    ).group(1),
+                ),
+            )
+        except shutil.Error:
+            pass
 
-for path in list(sorted_count.keys())[:-1]:
-    for _, folders, _ in os.walk(os.path.join(os.path.dirname(raw_path), path), topdown=False):
-        os.rmdir(os.path.join(os.path.dirname(raw_path), path, *folders))
+for release_folder in list(sorted_count.keys())[:-1]:
+    for dirpath, _, _ in os.walk(release_folder, topdown=False):
+        try:
+            os.rmdir(dirpath)
+        except OSError:
+            pass
